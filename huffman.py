8a276829e0decfd2a566461b315aec15c8d7dbe4
cheeese <tepliakov.pn@ucu.edu.ua>
2024-05-12 22:27:46 +0300

    lzw first version

---
+++ huffman.py
@@ -0,0 +1,214 @@
+def read_file(path: str, encoding: str) -> str:
+    with open(path, "rb") as file:
+        text = file.read().decode(encoding)
+    return text
+
+def str_to_bin(string: str) -> str:
+    return ''.join(format(ord(char), '08b') for char in string)
+
+
+
+class DoubleNode:
+    def __init__(self, data, prev = None, nxt = None) -> None:
+        self.data =data
+        self.prev = prev
+        self.next = nxt
+    
+    def __repr__(self) -> str:
+        output = f"{self.data}"
+        node = self.next
+        while node:
+            if node is self:
+                break
+            output += f" -> {node.data}"
+            if node is node.next:
+                break
+            node = node.next
+        return output
+
+    def __str__(self):
+        return repr(self)
+
+
+class Node:
+    def __init__(self, weight: int, chars: list[list[str]]):
+        self.weight = weight
+        self.chars = chars
+
+    def __lt__(self, other):
+        return self.weight < other.weight
+    
+    def __add__(self, other):
+        return Node(self.weight + other.weight, self.chars + other.chars)
+    
+    def __iter__(self):
+        return iter(self.chars)
+
+    def __repr__(self):
+        return f"node: {self.weight}, {self.chars}"
+
+class Huffman:
+    def __init__(self, block_size: int = 1) -> None:
+        self.block_size = block_size
+    @staticmethod
+    def count_frequency(text: str) -> dict[str, int]:
+        frequency = {}
+
+        for char in text:
+            frequency.setdefault(char, 0)
+            frequency[char] += 1
+
+        return frequency
+
+    def encode(self, text: str) -> tuple[str, dict[str, str]]:
+        if not text:
+            return '', {}
+
+        frequency = {}
+        for i in range(0, len(text), self.block_size):
+            cur_chars = text[i: i + self.block_size]
+            frequency[cur_chars] = frequency.get(cur_chars, 0) + 1
+
+        tree = [Node(weight, [[char,'']]) for char, weight in frequency.items()]
+        tree = sorted(tree, key = lambda node: node.weight)
+        tree_len = len(tree)
+        if tree_len == 1:
+            return '0'*frequency[text[0]], {text[0]: '0'}
+        head = DoubleNode(tree[0])
+        node = head
+        for el in tree[1:]:
+            node.next = DoubleNode(el, node)
+            node = node.next
+        last_node = None
+        while head and head.next:
+            first_min=head.data
+            second_min=head.next.data
+            head = head.next.next
+            if head:
+                head.prev = None
+            for char in first_min:
+                char[1] = '1' + char[1]
+            for char in second_min:
+                char[1] = '0' + char[1]
+            new_weight = first_min.weight + second_min.weight
+            if not head:
+                tree = [first_min + second_min]
+                break
+            elif new_weight <= head.data.weight:
+                head.prev = DoubleNode(first_min + second_min, None, head)
+                head = head.prev
+            else:
+                node = head if last_node is None else last_node
+                prev_node = None if last_node is None else last_node.prev
+                while node:
+                    if prev_node and prev_node.data.weight <= new_weight <= node.data.weight:
+                        prev_node.next = DoubleNode(first_min + second_min, prev_node, node)
+                        last_node = prev_node.next
+                        node.prev = last_node
+                        break
+                    if node.next is None:
+                        node.next = DoubleNode(first_min + second_min, node)
+                        last_node = node.next
+                        break
+                    prev_node = node
+                    node = node.next
+            tree_len -= 1
+        huffman_code = {rf"{item[0]}": item[1] for item in tree[0]}
+        encoded_text = ''.join(huffman_code[text[i: i + self.block_size]] for i in range(0, len(text), self.block_size))
+        return encoded_text, huffman_code
+
+    def decode(self, code: str, coding_dict: dict[str, str], byte = False) -> str:
+        decoded_text = ""
+        temp = ""
+        if byte:
+            zeros = 8 - len(bin(code[0])) + 2 if code else 0
+            myhex = code.hex()
+            myint = int(myhex, 16)
+            binary_string = "0"*zeros + "{:08b}".format(myint)
+            k = 0
+            end_k = 0
+            for char in binary_string:
+                end_k += 1
+                temp = binary_string[k: end_k]
+                if temp in coding_dict:
+                    decoded_text += coding_dict[temp]
+                    k = end_k
+                
+            return decoded_text
+        reverse=dict((v,k) for k,v in coding_dict.items())
+        for char in code:
+            temp += char
+            if temp in reverse:
+                decoded_text += reverse[temp]
+                temp = ""
+        return decoded_text
+
+    def encode_file(self, path: str, encoding: str):
+        with open(path, 'rb') as file:
+            text = file.read()
+            text_str = text.decode(encoding)
+
+        encoded_text, coding_dict = self.encode(text_str)
+
+        with open('encoded_'+path.split(".", 1)[0], 'wb') as file:
+            encoded_dict = ",".join(rf"{el}:{code}" for el, code in coding_dict.items())
+            bits_to_bytes = bytes(int(encoded_text[i:i+8],2) for i in range(0, len(encoded_text),8))
+            file.write((path.replace(";", "")+";"+encoded_dict+";").encode(encoding)+bits_to_bytes)
+        print("encoded")
+
+    def decode_file(self, path: str, encoding: str):
+        with open(path, "rb") as file:
+            text = file.read()
+        coding_text_dict = text.decode(encoding)
+        last_path = ""
+        coding_dict = {}
+        chars = ""
+        code = ""
+        no_dict_i = 0
+        creating_dict = False
+        code_time = False
+
+        for i, sym in enumerate(coding_text_dict):
+            if not creating_dict:
+                if sym == ";":
+                    creating_dict = True
+                    continue
+                last_path += sym
+            elif sym == "," and code_time:
+                code_time = False
+                coding_dict[code] = chars
+                code = ""
+                chars = ""
+            elif code_time:
+                if sym == ";":
+                    coding_dict[code] = chars
+                    no_dict_i = i
+                    break
+                code += sym
+            elif sym == ":" and coding_text_dict[i + 1] in "01":
+                code_time = True
+            else:
+                chars += sym
+        print("created_dict")
+        code_i = len(coding_text_dict[:no_dict_i].encode(encoding)) + 1
+        code = text[code_i:]
+
+        decoded_text = self.decode(code, coding_dict, True)
+
+        with open("decoded_" + last_path, "wb") as img:
+            img.write(decoded_text.encode(encoding))
+        print("decoded")
+
+import json
+
+if __name__ == "__main__":
+    huffman = Huffman(1)
+    name = "bug.png"
+    # file=read_file(name, "latin-1")
+    # text, dictionary = huffman.encode(file)
+    # print(text, type(text))
+    # print(dictionary, type(dictionary))
+    # dictstr = json.dumps(dictionary)
+    # print(str_to_bin(dictstr))
+    huffman.encode_file(name, "latin-1")
+    huffman.decode_file("encoded_"+name.split(".", 1)[0], "latin-1")
--- lzw.py
+++ lzw.py
@@ -1,63 +1,112 @@
-class LZW:
-    def encode(self, text: str) -> tuple[str, list]:
-        if not text:
-            return '', []
+from misc_funcs import read_file, dict_to_str, huffman_to_bytes, lst_to_bytes, bytes_to_lst
+from huffman import Huffman
 
-        dictionary={}
-        i=0
+class LZW:
+    def encode(self, text: str) -> tuple[list, list]:
+        if not text:
+            return "", []
+
+        dictionary = {}
+        i = 0
+        text1 = len(set(text))
+        counter = 0
 
         for char in text:
-            if len(dictionary)==len(set(text)):
+            if counter == text1:
                 break
             if char not in dictionary:
-                dictionary[char]=i
-                i+=1
-        
+                dictionary[char] = i
+                i += 1
+                counter += 1
+
         result_dictionary = list(dictionary.keys())
- 
-        encoded_text = ''
+
+        # encoded_text = ''
+        # encoded_text = []
+        encoded_text = b''
         temp = ""
+        counter = len(dictionary)
+        abcc=0
 
         for char in text:
             temp += char
             if temp not in dictionary:
-                encoded_text+=(str(dictionary[temp[:-1]]))+' '
-                dictionary[temp] = len(dictionary)
+                # encoded_text += (str(dictionary[temp[:-1]])) + " "
+                # encoded_text.append(dictionary[temp[:-1]])
+                encoded_text += dictionary[temp[:-1]].to_bytes(3, 'big')
+                print(abcc)
+                abcc+=1
+                dictionary[temp] = counter
+                counter += 1
                 temp = char
 
-        encoded_text+=str(dictionary[temp])
+        # encoded_text += str(dictionary[temp])
+        # encoded_text.append(dictionary[temp])
+        encoded_text += dictionary[temp].to_bytes(3, 'big')
 
         return encoded_text, result_dictionary
 
     def decode(self, code: str, coding_dict: list) -> str:
-        coding_dict = {i : char for i, char in enumerate(coding_dict)}
+        coding_dict = dict(enumerate(coding_dict))
         coding_dict_start = coding_dict.copy()
 
         decoded_text = ""
-        temp=''
-        code = list(map(int,code.split()))
+        temp = ""
+        code = list(map(int, code.split()))
 
         for num in code:
             if num in coding_dict:
                 decoded_text += coding_dict[num]
-                temp+=coding_dict[num]
+                temp += coding_dict[num]
                 if num in coding_dict_start:
                     if temp not in coding_dict.values():
                         coding_dict[len(coding_dict)] = temp
                         temp = temp[-1]
                 else:
-                    temp_2=''
+                    temp_2 = ""
                     for char in temp:
-                        temp_2+=char
+                        temp_2 += char
                         if temp_2 not in coding_dict.values():
                             coding_dict[len(coding_dict)] = temp_2
                             temp_2 = temp_2[:-1]
                             break
-                    temp = temp[len(temp_2):]
+                    temp = temp[len(temp_2) :]
             else:
-                temp=temp+temp[0]
+                temp = temp + temp[0]
                 coding_dict[len(coding_dict)] = temp
                 decoded_text += coding_dict[num]
         return decoded_text
-    
-    
+
+    def encode_file(self, path: str, encoding: str):
+        huffman = Huffman()
+
+        file_str = read_file(path, encoding)
+
+        encoded_text_huffman, coding_dict_huffman = huffman.encode(file_str)
+        print(len(encoded_text_huffman))
+        # coding_dict_huffman = dict_to_str(coding_dict_huffman)
+        # # print(encoded_text_huffman)
+        # # print(coding_dict_huffman)
+        # print(2)
+        # file_extension = path.split(".", 1)[1]
+        # file_name = path.split(".", 1)[0]
+
+        # encoded_text_lzw, coding_dict_lzw = self.encode(encoded_text_huffman)
+        # coding_dict_lzw = bytes(int(coding_dict_lzw[0]))
+        # # print(encoded_text_lzw)
+        # # print(coding_dict_lzw)
+
+        # # print(encoded_text_lzw)
+        # # print(coding_dict_lzw)
+        # print(1)
+        # # encoded_text_bytes, byte = lst_to_bytes(encoded_text_lzw)
+        # # print(encoded_text_bytes)
+
+        # with open(f"encoded_{file_name}.lzw", "wb") as file:
+        #     file.write(file_extension.encode(encoding) + b";" + coding_dict_lzw + b";" + encoded_text_lzw)
+
+
+if __name__ == "__main__":
+    lzw = LZW()
+    lzw.encode_file("bug.png", "latin-1")
+    # print(lzw.encode("abracadabraabraca"))
