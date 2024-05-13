77c2e666a8c39728bdfa7379245c443cfa15961d
cheeese <tepliakov.pn@ucu.edu.ua>
2024-05-13 20:52:05 +0300

    added decompression

--- lzw.py
+++ lzw.py
@@ -1,12 +1,22 @@
-'''LZW compression algorithm'''
+"""LZW compression algorithm"""
 
 from os import path as os_path
 
 class LZW:
+    '''LZW compression algorithm'''
+
+    @staticmethod
+    def get_extension(file: str) -> str:
+        '''Returns the extension of the file'''
+        with open(file, "rb") as f:
+            text = f.readline()
+        return text.split(b"|", maxsplit=1)[0].decode()
 
     def encode(self, file_bytes: bytes) -> tuple[bytes, int]:
+        '''Compresses the bytes'''
+
         if not file_bytes:
-            return b'', []
+            return b"", []
 
         dictionary = {bytes([i]): i for i in range(256)}
 
@@ -21,11 +31,13 @@ class LZW:
                 for char in file_bytes:
                     temp += bytes([char])
                     if temp not in dictionary:
-                        encoded_text.extend(dictionary[temp[:-1]].to_bytes(bytes_amount, 'big'))
+                        encoded_text.extend(
+                            dictionary[temp[:-1]].to_bytes(bytes_amount, "big")
+                        )
                         dictionary[temp] = counter
                         counter += 1
                         temp = bytes([char])
-                encoded_text.extend(dictionary[temp].to_bytes(bytes_amount, 'big'))
+                encoded_text.extend(dictionary[temp].to_bytes(bytes_amount, "big"))
                 break
             except OverflowError:
                 bytes_amount += 1
@@ -34,49 +46,74 @@ class LZW:
                 temp = b""
                 counter = 256
 
-
         return bytes(encoded_text), bytes_amount
 
+    def decode(self, code: bytes, bytes_per_code: int) -> bytes:
+        '''Decompresses the bytes'''
 
-    def decode(self, code: str, coding_dict: list) -> str:
-        coding_dict = dict(enumerate(coding_dict))
-        coding_dict_start = coding_dict.copy()
+        dictionary = {i: bytes([i]) for i in range(256)}
+        decoded_text = bytearray()
+        code_length = len(code)
 
-        decoded_text = ""
-        temp = ""
-        code = list(map(int, code.split()))
+        code_index = 0
+        next_code = 256
 
-        for num in code:
-            if num in coding_dict:
-                decoded_text += coding_dict[num]
-                temp += coding_dict[num]
-                if num in coding_dict_start:
-                    if temp not in coding_dict.values():
-                        coding_dict[len(coding_dict)] = temp
-                        temp = temp[-1]
-                else:
-                    temp_2 = ""
-                    for char in temp:
-                        temp_2 += char
-                        if temp_2 not in coding_dict.values():
-                            coding_dict[len(coding_dict)] = temp_2
-                            temp_2 = temp_2[:-1]
-                            break
-                    temp = temp[len(temp_2) :]
-            else:
-                temp = temp + temp[0]
-                coding_dict[len(coding_dict)] = temp
-                decoded_text += coding_dict[num]
-        return decoded_text
+        temp = b""
 
-    def encode_file(self, path: str):
+        while code_index < code_length:
+            current_code = int.from_bytes(
+                code[code_index : code_index + bytes_per_code], "big"
+            )
+            code_index += bytes_per_code
+
+            if current_code not in dictionary:
+                dictionary[current_code] = temp + bytes([temp[0]])
+
+            decoded_text += dictionary[current_code]
+
+            if temp:
+                dictionary[next_code] = temp + bytes([dictionary[current_code][0]])
+                next_code += 1
+
+            temp = dictionary[current_code]
+
+        return bytes(decoded_text)
+
+    def encode_file(self, path: str, file_name: str):
+        '''Compresses the file'''
+
         with open(path, "rb") as file:
             file_bytes = file.read()
 
         file = os_path.basename(path)
-        file_extension = file.split('.')[1]
+
+        try:
+            file_extension = file.split(".")[1]
+        except IndexError:
+            file_extension = ""
 
         encoded_text, bytes_amount = self.encode(file_bytes)
 
-        with open(f"encoded_{file}.lzw", 'wb') as file:
-            file.write(file_extension.encode()+b'|'+bytes_amount.to_bytes(1, 'big'+b'|'+encoded_text))
+        with open(file_name, "wb") as file:
+            file.write(
+                file_extension.encode()
+                + b"|"
+                + bytes_amount.to_bytes(1, "big")
+                + b"|"
+                + encoded_text
+            )
+
+    def decode_file(self, path: str, file_name: str):
+        '''Decompresses the file'''
+
+        with open(path, "rb") as file:
+            text = file.read()
+
+        file_extension, bytes_amount, encoded_text = text.split(b"|", 2)
+        file_extension = file_extension.decode()
+        bytes_amount = int.from_bytes(bytes_amount, "big")
+
+        decoded_text = self.decode(encoded_text, bytes_amount)
+
+        with open(file_name, "wb") as file:
+            file.write(decoded_text)
